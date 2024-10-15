a55c2d4c21a1f765ca39c5f9265b1e7ad98e4f3e
Mivinci <mivinci@qq.com>
2024-10-16 00:45:52 +0800

    :construction:  ovo: WIP

--- crates/ovo/src/context.rs
+++ crates/ovo/src/context.rs
@@ -1,9 +1,28 @@
+use crate::{Owned, Runtime, String, Value};
+use anyhow::Error;
 use ovo_quickjs::*;
 use std::ptr::NonNull;
 
 pub struct Context(pub(crate) NonNull<JSContext>);
 
+impl Context {
+  pub fn new(rt: &Runtime) -> Self {
+    let rt_raw = rt.inner.as_ptr();
+    let inner = match NonNull::new(unsafe { JS_NewContext(rt_raw) }) {
+      Some(inner) => inner,
+      None => panic!("null context"),
+    };
+    Self(inner)
+  }
+
+  pub fn eval(&self, source: String) -> Result<Owned<Value>, Error> {
+    _ = source;
+    unimplemented!()
+  }
+}
+
 impl Drop for Context {
+  #[inline(always)]
   fn drop(&mut self) {
     unsafe {
       JS_FreeContext(self.0.as_ptr());
@@ -13,6 +32,11 @@ impl Drop for Context {
 
 impl Clone for Context {
   fn clone(&self) -> Self {
-    unsafe { Context(NonNull::new(JS_DupContext(self.0.as_ptr())).unwrap()) }
+    let self_raw = self.0.as_ptr();
+    let inner = match NonNull::new(unsafe { JS_DupContext(self_raw) }) {
+      Some(inner) => inner,
+      None => panic!("null context"),
+    };
+    Context(inner)
   }
 }
--- crates/ovo/src/runtime.rs
+++ crates/ovo/src/runtime.rs
@@ -1,1 +1,16 @@
-pub struct Runtime;
+use ovo_quickjs::*;
+use std::ptr::NonNull;
+
+pub struct Runtime {
+  pub(crate) inner: NonNull<JSRuntime>, 
+}
+
+impl Runtime {
+  pub fn new() -> Self {
+    let inner = match NonNull::new(unsafe { JS_NewRuntime() }) {
+      Some(inner) => inner,
+      None => panic!("null runtime"),
+    };
+    Self { inner }
+  }
+}
