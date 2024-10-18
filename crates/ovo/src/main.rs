f68a6bb4bc7e288cb9c59b60f362a78d5c9d675c
mivinci <mivinci@qq.com>
2024-10-18 17:03:51 +0800

    🚧  ovo: play around clap

--- crates/ovo/src/main.rs
+++ crates/ovo/src/main.rs
@@ -1,3 +1,77 @@
+use clap::builder::styling::{AnsiColor, Color, Style};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "OvO", version, author)]
+#[command(about = "OvO - A lightweight JavaScript runtime.", long_about = None)]
+#[command(styles=new_styles())]
+struct Cli {
+  #[command(subcommand)]
+  command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+  /// Create an empty OvO project.
+  #[command()]
+  New {
+    /// Project name
+    name: String,
+  },
+
+  /// Add a dependency
+  #[command()]
+  Add {
+    /// Package name
+    name: String,
+  },
+
+  /// Remove a dependency
+  #[command()]
+  Remove {
+    /// Package name
+    name: String,
+  },
+
+  /// Run a file
+  #[command()]
+  Run {
+    /// Path to the file
+    path: Option<String>,
+  },
+
+  /// Run tests
+  #[command()]
+  Test {},
+
+  /// Compile a file
+  #[command()]
+  Compile {
+    /// Path to the file
+    path: Option<String>,
+  },
+
+  /// Serve an ES module
+  ///
+  /// The module should have at least a `fetch` function exported.
+  #[command()]
+  Serve {
+    /// Path to the module
+    path: Option<String>,
+  },
+}
+
 fn main() {
-  println!("Hello, world!");
+  let _ = Cli::parse();
+}
+
+fn new_styles() -> clap::builder::Styles {
+  clap::builder::Styles::styled()
+    .literal(
+      Style::new()
+        .bold()
+        .fg_color(Some(Color::Ansi(AnsiColor::Cyan))),
+    )
+    .usage(Style::new().bold())
+    .header(Style::new().bold())
 }
